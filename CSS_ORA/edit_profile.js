c88c784b5585c00270dc9c8df0da379ad8c51a5f
Thomas <dac.bui308@gmail.com>
2024-04-18 02:29:59 +0700

    Logging

--- CSS_ORA/edit_profile.js
+++ CSS_ORA/edit_profile.js
@@ -3,6 +3,10 @@ var info = {};
 fetch("./info.json")
   .then((response) => response.json())
   .then((info) => {
+    info = info;
+    console.log(
+      `information before changing (inside info.json file) : ${JSON.stringify(info)}`,
+    );
     document.getElementById("student-name").value = info.student.fullname;
     document.getElementById("gender").value = info.student.gender;
     document.getElementById("dantoc").value = info.student.dantoc;
@@ -42,13 +46,65 @@ document
   .getElementById("modifier-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    const field1Value = document.getElementById("student-name").value;
-    const field2Value = document.getElementById("phone").value;
+    var newInfo = {
+      student: {
+        fullname: document.getElementById("student-name").value,
+        gender: document.getElementById("gender").value,
+        joindate: "2020",
+        major: "Khoa học máy tính 2020",
+        school: "Trường Công nghệ thông tin và Truyền thông",
+        status: "Học",
+        stage: "Đại học đại trà",
+        class: "Khoa học máy tính 05-K65",
+        email: document.getElementById("email").value,
+        khoa: "65",
+        curTerm: "20231",
+        gpa: "4.0",
+        tc: "100",
+        tcfailed: "0",
+        eslevel: "4",
+        warning: "M0",
+        dantoc: "KINH",
+        hcgrad: document.getElementById("hcgrad").value,
+        location: "XÃ KHÁNH CƯ, HUYỆN YÊN KHÁNH, TỈNH NINH BÌNH",
+        viid: document.getElementById("viid").value,
+        issuedate: document.getElementById("issuedate").value,
+        idprovider: document.getElementById("idprovider").value,
+        religion: document.getElementById("religion").value,
+        highschool: document.getElementById("highschool").value,
+        phone: document.getElementById("phone").value,
+        quoctich: "Viet Nam",
+        birthplace: document.getElementById("birthplace").value,
+        doituong: document.getElementById("doituong").value,
+        city: document.getElementById("city").value,
+        quan: document.getElementById("quan").value,
+        xa: document.getElementById("xa").value,
+        homenumber: document.getElementById("homenumber").value,
+        curcity: document.getElementById("cur-city").value,
+        curquan: document.getElementById("cur-quan").value,
+        curxa: document.getElementById("cur-xa").value,
+        curhomenumber: document.getElementById("cur-homenumber").value,
+      },
+      father: {
+        name: document.getElementById("f-name").value,
+        DoB: document.getElementById("f-DoB").value,
+        job: document.getElementById("f-job").value,
+        phone: document.getElementById("f-phone").value,
+        email: document.getElementById("f-email").value,
+      },
+      mother: {
+        name: document.getElementById("m-name").value,
+        DoB: document.getElementById("m-DoB").value,
+        job: document.getElementById("m-job").value,
+        phone: document.getElementById("m-phone").value,
+        email: document.getElementById("m-email").value,
+      },
+    };
+
     fetch("./info.json")
       .then((response) => response.json())
       .then((data) => {
-        data.student.fullname = field1Value;
-        data.student.phone = field2Value;
+        data = newInfo;
         return fetch("./info.json", {
           method: "PUT", // or 'POST'
           headers: {
@@ -60,7 +116,7 @@ document
       .then(() => {
         alert("Information updated successfully!");
         // Redirect to display page
-        window.location.href = "./index.html";
+        console.log(`Info after update ${JSON.stringify(newInfo)}`);
       })
       .catch((error) => console.error("Error modifying data:", error));
   });
